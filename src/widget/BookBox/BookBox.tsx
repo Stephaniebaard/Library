176d6e5a66305c0af6670b6ae6cacf4b034b1ddf
Stephanie Baard <stephanie.baard@gmail.com>
2025-02-20 18:47:36 +0100

    New changes

---
+++ src/widget/BookBox/BookBox.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import BookForm from "./BookFom/BookForm"; 
+
+interface Book {
+  title: string;
+  author: string;
+  genre: string;
+  description: string;
+  coverImage: string;
+}
+
+const BookBox: React.FC = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  // Hantera bokinlämning från BookForm-komponenten
+  const handleBookSubmit = (book: Book) => {
+    setBooks([...books, book]); // Lägg till den nya boken i listan
+  };
+
+  return (
+    <div>
+      <h1>Min Boklista</h1>
+      <BookForm onSubmit={handleBookSubmit} /> 
+      <h3>Book List</h3>
+      <ul>
+        {books.map((book, index) => (
+          <li key={index}>
+            <h4>{book.title}</h4>
+            <p>{book.author}</p>
+            <p>{book.genre}</p>
+            <p>{book.description}</p>
+            <img src={book.coverImage} alt={`${book.title} cover`} style={{ width: '100px' }} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BookBox;
+
