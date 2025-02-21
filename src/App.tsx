05e01ced26e87a55c62c0dbb000d27898dfce640
jessicaagren <jessicaagren@hotmail.com>
2025-02-21 10:10:15 +0100

    Added AnimalList

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-
-import "./App.scss";
-import AuthorList from "./widget/AuthorList/AuthorList";
-import BookBox from "./widget/BookBox/BookBox";
-
+import './App.scss';
+import AnimalList from './widget/AnimalList/AnimalList';
+import AuthorList from './widget/AuthorList/AuthorList';
+import BookBox from './widget/BookBox/BookBox';
 
 function App() {
   return (
     <>
       <h1>Library📚</h1>
-      <AuthorList/>
-      <BookBox/>
+      <AuthorList />
+      <BookBox />
+      <AnimalList />
     </>
   );
 }
-
 
 export default App;
